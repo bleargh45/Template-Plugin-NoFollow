21925b297699edc9d8683833af1f8de08486ec44
Graham TerMarsch <graham@howlingfrog.com>
2007-09-30 22:45:22 +0000

    Copyright notice update.

--- lib/Template/Plugin/NoFollow.pm
+++ lib/Template/Plugin/NoFollow.pm
@@ -120,6 +120,8 @@ Graham TerMarsch <cpan@howlingfrog.com>
 
 =head1 COPYRIGHT
 
+Copyright (C) 2006-2007, Graham TerMarsch.  All Rights Reserved.
+
 This is free software; you can redistribute it and/or modify it under the same
 terms as Perl itself.
 
